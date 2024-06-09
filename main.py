2c26307b30dbed52a29fc5f1d4ffcb6be0e5bdc6
Victormuthomi <victormuhomi100@gmail.com>
2024-06-09 21:13:11 +0300

    Modify main.py.

--- main.py
+++ main.py
@@ -1,48 +1,40 @@
 import pywhatkit as kit
+import datetime
+import time
 
 class Text:
     """A model to send messages to my crush"""
-    def __init__(self,  phone_number, paragraph):
+    def __init__(self, phone_number, filename):
         """initialize the paragraph where the message to be sent is held"""
         self.phone_number = phone_number
-        self.paragraph = paragraph
+        with open(filename, 'r') as file:
+            self.paragraph = file.read().split('\n')
 
-    def first_line(self, message):
-        """"Sends the first message of the convesation"""
-        self.message = message
-        kit.sendwhatmsg(self.phone_number,self.message, 23, 10)
+    def send_messages(self):
+        """"Sends the messages of the conversation"""
+        # Send the first message
+        now = datetime.datetime.now()
+        hour, min = now.hour, now.minute
+        kit.sendwhatmsg(self.phone_number, "The messages are auto-sent. Please reply after they are over.", hour, min+1)
+        time.sleep(60)  # wait for 60 seconds
 
-    def line(self, line):
-         """Gets the lines from the paragraph"""
-         self.line = line
-         kit.sendwhatmsg(self.phone_number,self.line, 23, 25)
+        # Send each line of the paragraph every 2 minutes
+        for line in self.paragraph:
+            min += 2  # increment the minute by 2
+            if min > 59:
+                min -= 60
+                hour = (hour + 1) % 24
+            kit.sendwhatmsg(self.phone_number, line, hour, min)
+            time.sleep(120)  # wait for 120 seconds
 
+        # Send the last message
+        min += 2  # increment the minute by 2
+        if min > 59:
+            min -= 60
+            hour = (hour + 1) % 24
+        kit.sendwhatmsg(self.phone_number, "The messages are over. You can reply now.", hour, min)
 
-# Using 'with' to open the file
-with open('paragraph.txt', 'r') as file:
-    paragraph = file.read()
-
-# No need to call file.close() 
-
-#split the paragraph into different lines using a for loop
-lines =paragraph.split('\n')
-#save each line skipping empty lines
-for line in lines:
-    if line.strip():
-        line = line
-        
-paragraph = Text("+254734615998", paragraph )
-
-paragraph.line(line)
-
-
-
-
-
-
-
-
-
-
-
+# Replace 'text.txt' with the name of your file
+paragraph = Text("+254734615998", 'text.txt')
+paragraph.send_messages()
 
